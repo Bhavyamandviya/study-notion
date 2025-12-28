94bf4524ecf546d7c92f83af24bf2cb0ca57242c
Bhavyamandviya <bhavya17mandviya@gmail.com>
2025-12-28 22:13:11 +0530

    cc

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({});
+console.log("hello");
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
     return axiosInstance({
